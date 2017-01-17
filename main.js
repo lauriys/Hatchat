f888fc28af8ccf9d92994aafae18663e4e81bfba
Hatsune Miku <psqmaxor@gmail.com>
2017-01-17 21:07:15 +0100

    added an ugly join channel dialog that doesn't wor

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const electron = require('electron')
-// Module to control application life.
+
 const app = electron.app
-// Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
@@ -9,80 +8,60 @@ const url = require('url')
 
 const tmi = require('tmi.js')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, frame: false})
+function createWindow() {
+	mainWindow = new BrowserWindow({width: 800, height: 600, frame: false})
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes: true
+	}))
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+	mainWindow.on('closed', function () {
+		mainWindow = null
+	})
 
-  // require('electron-react-devtools').inject()
+	// require('electron-react-devtools').inject()
 
-  console.log('meow')
+	console.log('meow')
 
-  var client = new tmi.client({
-    channels: ['chaway']
-  })
+	var client = new tmi.client({
+		channels: []
+	})
 
-  client.connect()
+	client.connect()
 
-  client.on('chat', function(channel, userstate, message, self) {
-    mainWindow.webContents.send('message', {
-      channel: channel,
-      userstate: userstate,
-      message: message
-    })
-  })
+	client.on('chat', function(channel, userstate, message, self) {
+		mainWindow.webContents.send('message', {
+			channel: channel,
+			userstate: userstate,
+			message: message
+		})
+	})
 
-  client.on('join', function(channel, username, self) {
-    console.log(channel)
-    console.log(username)
-  })
+	client.on('join', function(channel, username, self) {
+		console.log('JOIN ' + channel + ' ' + username)
+	})
 
-  client.on('connected', function(address, port) {
-    console.log(address + ':' + port)
-  })
+	client.on('connected', function(address, port) {
+		console.log(address + ':' + port)
+	})
 
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+	if (mainWindow === null) {
+		createWindow()
+	}
 
 })
-console.log('nya')
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
--- src/components/ChannelBar.jsx
+++ src/components/ChannelBar.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
 
+import Modal from 'react-modal'
+
 var ChannelBar = React.createClass({
+
+	closeJoinModal: function() {
+		this.setState({
+			isJoinModalOpen: false
+		})
+	},
+
+	getInitialState: function() {
+		return {
+			isJoinModalOpen: false
+		}		
+	},
+
+	openJoinModal: function() {
+		this.setState({
+			isJoinModalOpen: true
+		})
+	},
+
 	render: function() {
 		return (
 			<div className="channels-wrapper flex-vertical">
@@ -14,9 +35,33 @@ var ChannelBar = React.createClass({
 					
 					<div className="channels-separator"></div>
 
+
 					<div className="channels-channel" style={{backgroundImage: "url('https://static-cdn.jtvnw.net/jtv_user_pictures/chaway-profile_image-46d2ea8ecd80ef36-300x300.png')", backgroundSize: '48px'}}></div>
 
+					<div onClick={this.openJoinModal} className="channels-join">
+						<i className="fa fa-plus"></i>
+					</div>
+
+
+
 				</div>
+				<Modal isOpen={this.state.isJoinModalOpen} style={{content: { height: '200px', width: '400px'}}} closeTimeoutMS={500} contentLabel="Test Modal" className="modal-content" overlayClassName="modal-overlay">
+					<div className="modal-title">
+						Join Channel
+					</div>
+
+					<div className="modal-padding">
+
+						<label htmlFor="channel">Channel name:</label>
+						<input name="channel" type="text" autoFocus></input>
+
+						<div className="modal-buttons">
+							<a className="button" onClick={this.closeJoinModal}>Cancel</a>
+							<a className="button button-primary" onClick={this.closeJoinModal}>Join</a>
+						</div>
+					</div>
+
+				</Modal>
 			</div>
 		)
 	}
