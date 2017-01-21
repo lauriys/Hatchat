ab7d82904309b977988c6f23ec44b6f74d42b749
Hatsune Miku <psqmaxor@gmail.com>
2017-01-21 12:22:15 +0100

    react devtools pls

--- renderer.js
+++ renderer.js
@@ -2,6 +2,7 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
+const isDev = /[\\/]electron[\\/]/.test(process.execPath)
 const remote = require('electron').remote
 const Menu = remote.Menu
 const MenuItem = remote.MenuItem
@@ -25,3 +26,12 @@ window.addEventListener('contextmenu', function(event) {
 	}
 	menu.popup(remote.getCurrentWindow())
 }, false)
+
+if(isDev) {
+	var electronDevToolsInstaller = require('electron-devtools-installer')
+	electronDevToolsInstaller.default(electronDevToolsInstaller.REACT_DEVELOPER_TOOLS).then(function(name) {
+		console.log('Loaded something: ', name)
+	}).catch(function(err) {
+		console.log('Error loading something: ', err)
+	})
+}
